31a7980d52eed540e2ccc2c586eb66055b2ebdf1
wcolts2000 <wcolts2000@gmail.com>
2019-02-05 10:53:56 -0800

    pre lunch push

--- backend/exerciseRoutes/exerciseRouter.js
+++ backend/exerciseRoutes/exerciseRouter.js
@@ -5,6 +5,7 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
+// POST AN EXERCISE CARD TO A JOURNAL
 router.post("/:journalId/exercises", auth.protected, (req, res, next) => {
   const {
     body: { name, reps, sets, weight },
@@ -24,6 +25,7 @@ router.post("/:journalId/exercises", auth.protected, (req, res, next) => {
     .catch(err => next(err));
 });
 
+// GET ALL EXERCISE CARDS FOR A JOURNAL
 router.get("/:journalId/exercises", auth.protected, (req, res, next) => {
   const { sub } = req.decodedToken;
   const { journalId } = req.params;
@@ -35,6 +37,18 @@ router.get("/:journalId/exercises", auth.protected, (req, res, next) => {
       console.log(exercises);
       res.send(exercises);
     })
+    .catch(err => next(err));
+});
+
+// GET SPECIFIC EXERCISE CARD
+router.get("/:journalId/exercises/:id", (req, res, next) => {
+  const { sub } = req.decodedToken;
+  const { journalId, id } = req.params;
+  db("exercise")
+    .where("exercise.journalId", journalId)
+    .where("exercise.id", id)
+    .where("exercise.userId", sub)
+    .then(exercise => res.send(exercise))
     .catch(err => next(err));
 });
 
